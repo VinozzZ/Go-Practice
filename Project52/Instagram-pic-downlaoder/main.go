2a6930d2c6dff962adb1308b2bfa47ad09b58652
VinozzZ <lionzhao0820@gmail.com>
2018-09-18 11:46:31 -0400

    Add go-instagram package to get data for a given user

--- Project52/Instagram-pic-downlaoder/main.go
+++ Project52/Instagram-pic-downlaoder/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"log"
 	"flag"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/gedex/go-instagram/instagram"
+	"github.com/joho/godotenv"
 )
 
 // 1. Get User in put from the command-line
@@ -11,12 +15,16 @@ import (
 var insName = flag.String("insName", "", "Please provide a instagram user name: ")
 var ClientID string
 
-func main() 
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	ClientID = os.Getenv("CLIENT_ID")
 	flag.Parse()
-	inputUser := *insName
-	fmt.Println("insName", inputUser)
+	// inputUser := *insName
+	client := instagram.NewClient(nil)
+	client.ClientID = ClientID
+	media, _, _ := client.Users.RecentMedia("357679952", nil)
+	fmt.Println("media", media)
 }
